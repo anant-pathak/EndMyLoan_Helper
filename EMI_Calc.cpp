10a0c421ac0fc7088e4cdafb258c45ad0fe44eae
anant pathak <anantpathak@anants-MBP.home>
2022-01-05 21:16:27 -0800

    Some bug and interface improvements

--- EMI_Calc.cpp
+++ EMI_Calc.cpp
@@ -1,40 +1,52 @@
 #include <iostream>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
 void emi_calculator(float &monthly, float &interest, float &amt, float &months_repay)
 {
+    std::cout<< "\t" << "Monthly" << "\t" << "ACH" << "\t" <<  "\t" << "Left" << endl;
     interest = ((interest/100)/12);
-    float payment_per_month;
+    float payment_per_month = (amt * (interest) * static_cast<float>(pow( (1+interest) ,  months_repay))) / 
+                            ( static_cast<float>(pow( (1+interest) ,  months_repay)) - 1);;
+    int i = 1;
+    float total_amt_paid = 0.0;
+    bool monthly_payment_valid = monthly;
+    cout << fixed << std::setprecision(2);
     while(amt > 0)
     {
-        payment_per_month = (amt * (interest) * static_cast<float>(pow( (1+interest) ,  months_repay))) / 
+        if( monthly_payment_valid )
+        {
+            payment_per_month = (amt * (interest) * static_cast<float>(pow( (1+interest) ,  months_repay))) / 
                             ( static_cast<float>(pow( (1+interest) ,  months_repay)) - 1);
+        }
         if(amt <= monthly)
         {
             monthly = amt - payment_per_month;
         }
+        total_amt_paid = total_amt_paid + payment_per_month + monthly;
         amt = amt - payment_per_month - monthly;
         months_repay--;
-        cout << monthly << "\t" << payment_per_month << "\t" << amt << endl;
+        cout << i++ << "\t" <<monthly << "\t" << payment_per_month << "\t" <<  "\t" << (amt >= 0 ? amt : 0) << endl;
     }
+    cout <<"Total amount you paid = " << total_amt_paid << endl;
 }
 
 int main()
 {
-    std::cout<< "Monthly" << "\t" << "ACH" << "\t" << "Left" << endl;
+    
     float monthly;
     float interest_yearly;
     float amt;
     float months_to_repay;
     cout << "\nMonthly  =  ? "; 
     cin >> monthly;
-    cout << "\nInterest/year = ?";
+    cout << "\nInterest/year = ? ";
     cin >> interest_yearly;
-    cout << "\nCurrent amt = ?";
+    cout << "\nCurrent amt = ? ";
     cin >> amt;
-    cout << "\nTotal months to repay = ?" ; 
+    cout << "\nTotal months to repay = ? " ; 
     cin >> months_to_repay;
     cout << endl;
     emi_calculator(monthly, interest_yearly, amt, months_to_repay);
